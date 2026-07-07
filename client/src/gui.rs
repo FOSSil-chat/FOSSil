ebd772408de0e4e1acf88430e47860c7ec0bf453
TsarGalxyGaming <tsargalxygaming@void.broadband>
2026-07-07 20:20:08 +0100

    implementing gui

--- client/src/gui.rs
+++ client/src/gui.rs
@@ -1,32 +1,26 @@
-use eframe::egui;
-use std::sync::mpsc::Receiver;
-
-pub fn main(_rx: Receiver<String>) {
-    // Remove _ when communicating between GUI and network.rs (do same in network.rs) - tx is sender and rx is receiver
-    let native_options = eframe::NativeOptions::default();
-    let _ = eframe::run_native(
-        "FOSSil Chat",
-        native_options,
-        Box::new(|cc| Ok(Box::new(FOSSilChat::new(cc)))),
-    );
-}
+use eframe::egui::CentralPanel;
 
 #[derive(Default)]
-struct FOSSilChat {}
 
-impl FOSSilChat {
-    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        //persistence, set global style, set global font, graphics shaders/buffers
+struct App {}
 
-        Self::default()
+impl eframe::App for App {
+    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| ui.heading("Hello World"));
     }
 }
 
-impl eframe::App for FOSSilChat {
-    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ui, |ui| {
-            ui.heading("Hello World!");
-            // widgets
-        });
-    }
+pub fn main() -> Result<(), eframe::Error> {
+    println!("Hello World!");
+    let options = eframe::NativeOptions {
+        viewport: eframe::egui::ViewportBuilder::default()
+            .with_resizable(true)
+            .with_inner_size([1366.0, 768.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "FOSSil Chat",
+        options,
+        Box::new(|_cc| Ok(Box::<App>::default())),
+    )
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,14 +1,14 @@
-//use fossil_client::gui;
+use fossil_client::gui;
 use fossil_client::network;
 
 use std::sync::mpsc;
 
 fn main() {
+    gui::main();
+
     let (tx, _rx) = mpsc::channel(); // Allows communication between threads, temporarily unused
 
     //std::thread::spawn(move || {
     network::run(tx);
     //});
-
-    /*gui::main(rx);*/
 }
