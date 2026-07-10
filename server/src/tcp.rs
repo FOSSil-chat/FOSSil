a4b444c57f29842f7f87911595ab9618d0b35ec8
Simarpreet Singh <simarsaini13@gmail.com>
2026-07-10 21:43:21 +0100

    Fix formatting from previous commit

--- server/src/tcp.rs
+++ server/src/tcp.rs
@@ -17,7 +17,7 @@ fn set_keepalive(stream: TcpStream) -> std::io::Result<TcpStream> {
     socket.set_tcp_keepalive(
         &TcpKeepalive::new()
             .with_time(Duration::from_secs(60))
-            .with_interval(Duration::from_secs(10))
+            .with_interval(Duration::from_secs(10)),
     )?;
 
     socket.set_nonblocking(true)?;
