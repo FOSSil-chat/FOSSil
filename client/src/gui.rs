c411e63f6b9601c8dc67e38961f4854a1518a332
TsarGalxyGaming <tsargalxygaming@void.broadband>
2026-07-11 12:12:26 +0100

    pinpointing design language

--- client/src/gui.rs
+++ client/src/gui.rs
@@ -14,9 +14,9 @@ pub fn launch() {
 
 #[component]
 fn App() -> Element {
-    let fonts = format!(
+        let fonts = format!(
         "@font-face {{
-            font-family: 'Liberation Sans Regular';
+            font-family: 'Liberation Sans';
             src: url('{REGULAR}') format('truetype');
             font-weight: normal;
         }}
@@ -27,39 +27,36 @@ fn App() -> Element {
         }}"
     );
 
-    let rsx_style = format!(
-        "width: 100%;
-         height: 100vh;
-         background-image: url('{BG_IMAGE}');
-         background-size: cover;
-         background-position: center;
-         background-repeat: no-repeat;
-         display: flex;
-         flex-direction: column;
-         justify-content: center;
-         align-items: flex-end;
-         padding-right: 40px;"
-    );
-
     rsx! {
 
         style { "{fonts}" }
 
         div {
 
-            style: "{rsx_style}",
+            style: "position: relative; width: 100vw; height: 100vh; overflow: hidden;",
 
-            p {
-                style: "font-family: 'Liberation Sans'; font-weight: bold; font-size: 24px;",
-                "FOSSil Chat"
+            img {
+                src: "{BG_IMAGE}",
+                style: "position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: fill;"
             }
+            
+            div {
 
-            p {
-                style: "font-family: 'Liberation Sans'; font-weight: normal;",
-                "Welcome to FOSSil Chat!"
+                style: "position: absolute; top: 0; left: 0; width: 100%; height: 100%;
+                         display: flex; flex-direction: column;
+                         justify-content: center; align-items: flex-end;
+                         gap: 2vh; padding: 4vw; box-sizing: border-box;", 
+                p {
+                    style: "font-family: 'Liberation Sans'; font-weight: bold; font-size: 2.05vw;",
+                    "FOSSil Chat"
+                }
+
+                p {
+                    style: "font-family: 'Liberation Sans'; font-weight: normal; font-size: 1.25vw;",
+                    "Welcome to FOSSil Chat!"
+                }
+
             }
-
-
 
 
         }
